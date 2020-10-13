3ce2fe49ef434a62d5f00a6082e5deb3b8fd30f9
amandaymxs <amandaxinvestment@gmail.com>
2020-10-13 16:47:21 -0400

    main, testing code

---
+++ ATM.java
@@ -0,0 +1,19 @@
+
+public class ATM {
+
+	public static void main(String[] args) {
+		BankAccount newAccount = new BankAccount();
+		newAccount.deposit(200);
+		newAccount.withdrawal(-10);
+		newAccount.deposit(20);
+		newAccount.deposit(10);
+		newAccount.deposit(30);
+		newAccount.withdrawal(-100);
+		newAccount.deposit(15);
+		newAccount.deposit(20);
+		newAccount.deposit(10);
+		newAccount.printTenPreviousTransactions();
+		
+	}
+
+}
