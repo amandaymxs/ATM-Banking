3e8a2046189b9085cd5a36d31e536be6a2f7eaff
amandaymxs <amandaxinvestment@gmail.com>
2020-10-12 22:27:36 -0400

    double balance, default constructor, withdrawal ifelse

--- BankAccount.java
+++ BankAccount.java
@@ -1,15 +1,44 @@
+import java.util.Random;
 
 public class BankAccount {
 
-	private int balance;
+	private double balance;
 	private String customerName;
-	private int customerID;
+	private String customerID;
 	private int size = 100;
 	private double[] previousTransaction = new double[size];
 	private String[] previousTransactionDetails = new String[size];
 	private double[] previousBalance = new double[size]; 
+	Random random = new Random();
 	
 	int counter = 0;
+	
+	public BankAccount(double balance, String customerName, String customerID) {
+		this.balance = 0;
+		this.customerName = "Mickey";
+		this.customerID = "M00000";
+	}
+	
+	public void withdrawal(double num) {
+		num = Math.round(num*100)/100;		//round to two decimal places
+		if (num > 0) {
+			System.out.println("Error 10005: Enter a negative amount.");
+		} else if ((-num) > balance){
+			System.out.println("Error: 10007: Your account balance cannot be negative."
+					+ "Your withdrawal cannot exceed " + balance + ".");
+		} else {
+				balance += num;
+		}
+	}
+	
+	public void deposit(double num) {
+		num = Math.round(num*100)/100;		//round to two decimal places
+		if (num < 0) {
+			System.out.println("Error 10006: Enter a positive amount.");
+		} else {
+			balance += num;
+		}
+	}
 	
 	public int getBalance() {
 		return balance;
@@ -19,10 +48,19 @@ public class BankAccount {
 		return customerName;
 	}
 
-	public int getCustomerID() {
+	public String getCustomerID() {
 		return customerID;
 	}
+	
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName.toUpperCase();
+		setCustomerID(this.customerName);
+	}
 
+	public void setCustomerID(String customerName) {
+		this.customerID = customerName.substring(0) + random.nextInt(100000);
+	}
+	
 	public void previousTransactions() {
 		System.out.println(previousTransactionDetails[counter-1]
 				+ "\t" + previousTransaction[counter-1] + "\t"
@@ -38,4 +76,8 @@ public class BankAccount {
 		}
 	}
 	
+	public void exitTransaction() {
+		System.out.println(customerName + " (" + customerID +
+				") has successfully exited transaction.");
+	}
 }
