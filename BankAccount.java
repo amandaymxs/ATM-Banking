9aeb3c22ac529801b8f65724e316b7f8d2816bcd
amandaymxs <amandaxinvestment@gmail.com>
2020-10-12 21:47:58 -0400

    add print transactions method

--- BankAccount.java
+++ BankAccount.java
@@ -2,8 +2,6 @@
 public class BankAccount {
 
 	private int balance;
-	private int transaction;
-	private String transactionDetail;
 	private String customerName;
 	private int customerID;
 	private int size = 100;
@@ -17,12 +15,27 @@ public class BankAccount {
 		return balance;
 	}
 	
+	public String customerName() {
+		return customerName;
+	}
+
+	public int getCustomerID() {
+		return customerID;
+	}
+
 	public void previousTransactions() {
 		System.out.println(previousTransactionDetails[counter-1]
-				+ "\t" + previousTransaction[counter-1]);
+				+ "\t" + previousTransaction[counter-1] + "\t"
+				+ previousBalance[counter-1]);
 	}
 	
 	public void printTenPreviousTransactions() {
-		System.out.println();
+		System.out.println("Previous Transactions Newest to Oldest");
+		for(int i = counter; i > (counter - 10); i--) {
+			System.out.println(previousTransactionDetails[i]
+					+ "\t" + previousTransaction[i] + "\t"
+					+ previousBalance[i]);
+		}
 	}
+	
 }
