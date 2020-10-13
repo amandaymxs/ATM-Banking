ad9ba95dcd85e15f61a3a2320c97311d4c8f6cb7
amandaymxs <amandaxinvestment@gmail.com>
2020-10-13 11:54:15 -0400

    Verify Array Size

--- BankAccount.java
+++ BankAccount.java
@@ -5,21 +5,27 @@ public class BankAccount {
 	private double balance;
 	private String customerName;
 	private String customerID;
-	private int size = 100;
+	private int size = 10000;
 	private double[] previousTransaction = new double[size];
 	private String[] previousTransactionDetails = new String[size];
 	private double[] previousBalance = new double[size]; 
 	Random random = new Random();
-	
 	int counter = 0;
 	
+	/*---------------------------------------------------------------------------------------------
+	* Default Constructor
+	---------------------------------------------------------------------------------------------*/
 	public BankAccount(double balance, String customerName, String customerID) {
 		this.balance = 0;
 		this.customerName = "Mickey";
 		this.customerID = "M00000";
 	}
 	
+	/*---------------------------------------------------------------------------------------------
+	 * Transactions
+	 ---------------------------------------------------------------------------------------------*/
 	public void withdrawal(double num) {
+		verifyArraySize();
 		num = Math.round(num*100)/100;		//round to two decimal places
 		if (num > 0) {
 			System.out.println("Error 10005: Enter a negative amount.");
@@ -32,6 +38,7 @@ public class BankAccount {
 	}
 	
 	public void deposit(double num) {
+		verifyArraySize();
 		num = Math.round(num*100)/100;		//round to two decimal places
 		if (num < 0) {
 			System.out.println("Error 10006: Enter a positive amount.");
@@ -40,11 +47,27 @@ public class BankAccount {
 		}
 	}
 	
+	/*-----------------------------------------------------------------------------------
+	 * Setters
+	 ------------------------------------------------------------------------------------*/
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName.toUpperCase();
+		setCustomerID(this.customerName);	// Auto generates customerID
+	}
+
+	public void setCustomerID(String customerName) {
+		this.customerID = customerName.substring(0) + random.nextInt(100000);
+	}
+	
+	/*---------------------------------------------------------------------------------
+	 * Getters
+	 ----------------------------------------------------------------------------------*/
+	
 	public double getBalance() {
 		return balance;
 	}
 	
-	public String customerName() {
+	public String getCustomerName() {
 		return customerName;
 	}
 
@@ -52,16 +75,10 @@ public class BankAccount {
 		return customerID;
 	}
 	
-	public void setCustomerName(String customerName) {
-		this.customerName = customerName.toUpperCase();
-		setCustomerID(this.customerName);
-	}
-
-	public void setCustomerID(String customerName) {
-		this.customerID = customerName.substring(0) + random.nextInt(100000);
-	}
-	
-	public void previousTransactions() {
+	/*---------------------------------------------------------------------------------------
+	 * Print
+	 ----------------------------------------------------------------------------------------*/
+	public void printPreviousTransactions() {
 		System.out.println(previousTransactionDetails[counter-1]
 				+ "\t" + previousTransaction[counter-1] + "\t"
 				+ previousBalance[counter-1]);
@@ -76,6 +93,16 @@ public class BankAccount {
 		}
 	}
 	
+	/*------------------------------------------------------------------------------------------
+	 * End Transaction
+	 ------------------------------------------------------------------------------------------*/
+	public void verifyArraySize() {
+		if (counter == size) {
+			System.out.println("Error 30000: Transaction limit exceeded."
+					+ " Transaction unsuccessful.");
+			System.exit(0);
+		}
+	}
 	public void exitTransaction() {
 		System.out.println(customerName + " (" + customerID +
 				") has successfully exited transaction.");
